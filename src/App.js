5e1faca40ae22eef390d5480059a2bac8b26f5e8
Muzammilishtiaq <167680670+Muzammilishtiaq@users.noreply.github.com>
2024-05-10 16:38:53 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
           <Header />
             <Routes>
                 <Route path='/' element={<Home />}></Route>
-                <Route path="/:id" element={<Movie />}></Route>
-                <Route path="/:type" element={<MovieList />}></Route>
+                <Route path="/movie/:id" element={<Movie />}></Route>
+                <Route path="/movies/:type" element={<MovieList />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
             </Routes>
         </Router>
